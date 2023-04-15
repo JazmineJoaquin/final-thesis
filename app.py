106d8540a85e1361d2d6e1ef89f468c5ff26b92a
Jazmine Joaquin <82792981+JazmineJoaquin@users.noreply.github.com>
2023-04-15 21:43:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -145,5 +145,6 @@ def disconnect():
     print(f"{name} has left the room {room}")
 
 
-if __name__ == '__main__':
-    socketio.run(app)
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
+    socketio = SocketIO(app, cors_allowed_origins="*")
