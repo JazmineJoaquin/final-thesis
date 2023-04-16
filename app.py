9e916eef2a7f4f06939f5b79a3bdc6932f68842b
Jazmine Joaquin <82792981+JazmineJoaquin@users.noreply.github.com>
2023-04-16 20:25:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -143,4 +143,4 @@ def disconnect():
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
-    socketio = SocketIO(app, async_mode='gevent', cors_allowed_origins="*")
+    socketio = SocketIO(app, async_mode='gevent')
